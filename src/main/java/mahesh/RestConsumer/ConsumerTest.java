93f4e7a2f398e5c9e6c0e29f187f3f0b5259756c
Mahesh <dadimahesh972@gmail.com>
2022-01-25 20:55:25 +0530

    first commit

---
+++ src/main/java/mahesh/RestConsumer/ConsumerTest.java
@@ -0,0 +1,27 @@
+package mahesh.RestConsumer;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class ConsumerTest {
+	
+	public static void main(String[] args) {
+		
+		String url="http://localhost:2525/RestProviderApp/rest/provider/msg";
+		
+		Client c=Client.create();
+		
+		WebResource wr=c.resource(url);
+		
+		ClientResponse cr=wr.get(ClientResponse.class);
+		
+		String str=cr.getEntity(String.class);
+		
+		System.out.println(str);
+		
+		System.out.println(cr.getStatus());
+		System.out.println(cr.getStatusInfo());
+	}
+
+}
